8e570b7fb03466512f3494c2461ae5c6e79af34f
vice-computer <1067049538@qq.com>
2022-04-19 11:46:57 +0800

    1

--- SE.py
+++ SE.py
@@ -4,12 +4,16 @@ from cmath import exp
 import numpy as np
 import math
 import random
-
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
 
 Temperture = 10000
 dT = 0.98
 eps = 1e-6
 y = 66
+fitness_list = []
+iteration=0
 
 
 def fitness(x):
@@ -18,10 +22,13 @@ def fitness(x):
 
 def simulated_annealing():
     global Temperture
+    global iteration
+    global fitness_list
     x = random.uniform(0, 100)
     n = fitness(x)
     answer = n
     while(Temperture > eps):
+        iteration+=1
         new_x = x+Temperture*random.uniform(-100, 100)
         if new_x >= 0 and new_x <= 100:
             new_y = fitness(new_x)
@@ -32,8 +39,11 @@ def simulated_annealing():
             elif(random.random() < math.exp((new_y-n)/Temperture)):
                 n = new_y
                 x = new_x
+        fitness_list.append(new_y)
         Temperture *= dT
     return answer
 
 
 print(simulated_annealing())
+print(iteration)
+print(len(fitness_list))
